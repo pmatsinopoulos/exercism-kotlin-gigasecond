22bcd5a638393af636a1c28ab7200c23d06f283d
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-04 14:06:59 +0300

    Use one line to declare the getter

--- src/main/kotlin/Gigasecond.kt
+++ src/main/kotlin/Gigasecond.kt
@@ -4,9 +4,7 @@ import java.time.LocalTime
 
 class Gigasecond(localDateTime: LocalDateTime) {
     var date: LocalDateTime = localDateTime
-        get() {
-            return field.plusSeconds(1_000_000_000)
-        }
+        get() = field.plusSeconds(1_000_000_000)
 
     constructor(localDate: LocalDate) : this(LocalDateTime.of(localDate.year, localDate.month, localDate.dayOfMonth, 0, 0, 0))
 }
