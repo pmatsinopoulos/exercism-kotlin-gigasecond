5d342bfacd15f4e0ae686cb469616f2adc8f7384
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-04 14:13:39 +0300

    Calculate the correct date at the moment you construct. Use the .atStartOfDay() to convert LocalDate to LocalDateTime

--- src/main/kotlin/Gigasecond.kt
+++ src/main/kotlin/Gigasecond.kt
@@ -3,8 +3,7 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 
 class Gigasecond(localDateTime: LocalDateTime) {
-    var date: LocalDateTime = localDateTime
-        get() = field.plusSeconds(1_000_000_000)
+    var date: LocalDateTime = localDateTime.plusSeconds(1_000_000_000)
 
-    constructor(localDate: LocalDate) : this(LocalDateTime.of(localDate.year, localDate.month, localDate.dayOfMonth, 0, 0, 0))
+    constructor(localDate: LocalDate) : this(localDate.atStartOfDay())
 }
